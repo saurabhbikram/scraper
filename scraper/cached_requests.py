a682c127e83aa61cb57145a9c308d44139e5300a
saurabhbikram <sbikram@jha.com>
2020-09-15 10:37:05 +0100

    enable multiproc

--- scraper/cached_requests.py
+++ scraper/cached_requests.py
@@ -5,9 +5,8 @@ import logging
 from typing import List, Dict
 import time, json
 from datetime import datetime, timedelta
-import s3fs
-
-PROFILE = os.getenv('AWSACC')
+import boto3
+import boto3.session
 
 logging.basicConfig()
 log = logging.getLogger()
@@ -15,17 +14,30 @@ log = logging.getLogger()
 def default_engine(host):
     return db.create_engine(host, poolclass=db.pool.NullPool)
 
+aws_session = boto3.session.Session(profile_name='sb')
+s3 = aws_session.client('s3')
+
+def read_aws(bucket, k):
+    obj = s3.get_object(Bucket=bucket, Key=k)
+    with gzip.GzipFile(fileobj=obj["Body"]) as gzipfile:
+        content = gzipfile.read()
+    return content
+
+def write_aws(bucket, k, content):
+    res_gz = gzip.compress(content)
+    res = s3.put_object(Body=res_gz, Bucket=bucket, Key=k)
+    assert res['ResponseMetadata']['HTTPStatusCode'] == 200
+
 class CReq():
-    def __init__(self, engine: db.engine=None, cache_loc: str="", proxies: List[str]=None):
+    def __init__(self, engine: db.engine=None, bucket: str="sbcrawl-test", proxies: List[str]=None):
         super().__init__()
         self.dbs = {'engine':engine}
-        self.s3 = s3fs.S3FileSystem(profile=PROFILE)
         if engine is not None:
             self.dbs['metadata'] = db.MetaData()
             self.dbs['pages'] = db.Table('pages', self.dbs['metadata'], autoload=True, autoload_with=engine)
         self.proxies = proxies
-        self.cache_loc = cache_loc
         self.requests_session = requests.Session()
+        self.bucket = bucket
     
     def get_proxy(self):
         proxies = self.proxies
@@ -36,17 +48,15 @@ class CReq():
         else: proxy_dict = None
         return proxy_dict
 
-    def get_www(self, url):
+    def get_www(self, url, raise_for_status=True):
         num_tries = 0
         while True:
             try:
                 pxy = self.get_proxy()
                 res = requests.get(url, proxies=pxy)
-                # only raise status if code is not 404
-                if res.status_code != 404: res.raise_for_status()
+                if raise_for_status: res.raise_for_status()
                 break
-            except (requests.exceptions.ProxyError, requests.exceptions.HTTPError, 
-                    requests.exceptions.SSLError, requests.exceptions.ChunkedEncodingError) as e:
+            except (requests.exceptions.ProxyError, requests.exceptions.HTTPError, requests.exceptions.SSLError) as e:
                 log.warning(f"Get {num_tries+1}/10 failed with proxy on {url} and proxy {pxy['http']}")
                 num_tries += 1
                 if num_tries == 9:
@@ -70,26 +80,19 @@ class CReq():
                           )).\
                 order_by(db.desc(pages.columns.date_created))
         self.dbs['engine'].dispose()
-        while True:
-            try:
-                with self.dbs['engine'].connect() as connection: 
-                    qry_exec = connection.execute(query)
-                    res = qry_exec.fetchall()
-                break
-            except db.exc.OperationalError as e:
-                time.sleep(1)
+        with self.dbs['engine'].connect() as connection: 
+            qry_exec = connection.execute(query)
+            res = qry_exec.fetchall()
         
         return res
     
     def read_file(self, id:int, content_ext:str="html"):
-        if self.cache_loc == "": return None
-        path = os.path.join(self.cache_loc, str(id))
+        path = str(id)
         res = t = type('', (), {})()
         res.file_loc = path
         try:
-            with self.s3.open(f'{path}.gz.{content_ext}', 'rb') as f:
-                g = gzip.GzipFile(fileobj=f)
-                file_content = g.read()
+            k = f'{str(id)}.gz.{content_ext}'
+            file_content = read_aws(self.bucket, k)
             res.content = file_content
             res.status_code = 200
         except FileNotFoundError as e:
@@ -100,8 +103,7 @@ class CReq():
     def get_db(self, url, post_msg=None):
         res_list = self.get_db_id(url, post_msg)
         # TO DO: return multiple objects if more than 1 match
-        all_res = []
-        for db_id, db_headers, db_date_created in res_list: 
+        for db_id, db_headers, db_date_created in res_list[:1]: 
             if db_headers is not None:
                 db_headers = {x.lower().strip():v for x,v in db_headers.items()}
                 if db_headers['content-type'].find('html') > 0:
@@ -115,18 +117,14 @@ class CReq():
                 content_ext = "html"
             
             res = self.read_file(db_id, content_ext)
-            if res.status_code == 404: continue
             res.url = url
             res.headers = db_headers
             res.date_created = db_date_created
             if content_ext == "json":
                 res.json = json.loads(res.content)
-            all_res.append(res)
-            
-        return all_res
+            return res
 
     def save_db(self, res, post_msg=None):
-        if self.cache_loc == "": return None
         pages = self.dbs['pages']
         self.dbs['engine'].dispose()
         url_save = res.post_url if hasattr(res,'post_url') else res.url
@@ -138,10 +136,7 @@ class CReq():
             try:
                 dres = connection.execute(query)
                 id_save = dres.fetchone()[0]
-                path = os.path.join(self.cache_loc, str(id_save))
-                with self.s3.open(f'{path}.gz.html', 'wb') as f:
-                    res_gz = gzip.compress(res.content)
-                    f.write(res_gz)
+                write_aws(self.bucket, str(id_save)+'.gz.html',res.content)                
                 trans.commit()
             except:
                 log.warning(f"ID {str(id_save)} could not save, will not commit to db.")
@@ -149,35 +144,25 @@ class CReq():
                 raise
         return None
     
-    def get(self, url, max_age_days=365) -> List[object]:
-        """
-        Gets a list of request objects from cache. 
-        If max_age_days is 0, then also fetches the latest from www.
-        The order is based on date fetched, so most recent is always first
-        """
+    def get(self, url, max_age_days=365):
+
         # if no engine specified, act like normal requests
         if self.dbs['engine'] is None: return self.get_www(url)
 
-        db_res = []
-        www_res = []
+        res = None
         force_new = (max_age_days<1)
-
-        db_res = self.get_db(url)
-        if len(db_res):
-            # check date created of most recent `get` to see if want to get a newer one
-            if db_res[0].status_code == 404 or\
-                db_res[0].date_created < (datetime.today() - timedelta(days=max_age_days)): 
-                force_new = True
+        if not force_new: 
+            res = self.get_db(url)
+            if res is not None:
+                if res.status_code == 404 or\
+                    res.date_created < (datetime.today() - timedelta(days=max_age_days)): 
+                    force_new = True
                 
-        if force_new or not len(db_res):
-            log.info("Getting from www")
-            www_res = self.get_www(url)
-            www_res.headers['final_url'] = www_res.url
-            www_res.url = url
-            self.save_db(www_res)
-            www_res = [www_res]
-
-        return www_res + db_res
+        if res is None or force_new:
+            log.info("Not found in database, getting from web")
+            res = self.get_www(url)
+            self.save_db(res)
+        return res
     
     def post(self, url, data:dict, request_headers=None, force_new=False):
 
--- scraper/crawler.py
+++ scraper/crawler.py
@@ -1,9 +1,9 @@
-from multiprocessing.pool import ThreadPool
+from multiprocessing.pool import Pool
 import tqdm
 
 def crawl_urls(func, urls, threads=2):
     if threads > 1:
-        with ThreadPool(processes=threads) as p:
+        with Pool(processes=threads) as p:
             res = list(tqdm.tqdm(p.imap(func, urls), total=len(urls)))
     else:
         res = []
