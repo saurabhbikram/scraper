be7e95acb73ef8c8ad7be02fd04b41afd6383729
saurabhbikram <sbikram@jha.com>
2020-12-26 12:20:11 +0000

    add desc to tqdm

--- scraper/crawler.py
+++ scraper/crawler.py
@@ -1,11 +1,11 @@
 from multiprocessing.pool import ThreadPool
 import tqdm
 
-def crawl_urls(func, urls, threads=2):
+def crawl_urls(func, urls, threads=2, desc=""):
     if threads > 1:
         with ThreadPool(processes=threads) as p:
-            res = list(tqdm.tqdm(p.imap(func, urls), total=len(urls)))
+            res = list(tqdm.tqdm(p.imap(func, urls), total=len(urls), desc=desc))
     else:
         res = []
-        for u in tqdm.tqdm(urls): res.append(func(u))
+        for u in tqdm.tqdm(urls, desc=desc): res.append(func(u))
     return res
