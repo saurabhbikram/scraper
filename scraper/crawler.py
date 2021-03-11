acc6bd9fcafa13cceb3455515e118a13aa9e300e
saurabhbikram <sbikram@jha.com>
2021-03-11 11:55:56 +0000

    progress option

--- scraper/crawler.py
+++ scraper/crawler.py
@@ -1,11 +1,16 @@
 from multiprocessing.pool import ThreadPool
 import tqdm
 
-def crawl_urls(func, urls, threads=2, desc=""):
+
+def crawl_urls(func, urls, threads=2, desc="", progress=True):
     if threads > 1:
         with ThreadPool(processes=threads) as p:
-            res = list(tqdm.tqdm(p.imap(func, urls), total=len(urls), desc=desc))
+            if progress:
+                res = list(tqdm.tqdm(p.imap(func, urls), total=len(urls), desc=desc))
+            else:
+                res = p.map(func, urls)
     else:
         res = []
-        for u in tqdm.tqdm(urls, desc=desc): res.append(func(u))
+        for u in tqdm.tqdm(urls, desc=desc):
+            res.append(func(u))
     return res
